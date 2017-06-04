bba71622fd0f2ed3d29e0eff47242998f56761a2
root <root@srvkerber.kerber.intra>
2017-06-05 01:03:43 +0200

    Fixed various syntax errors

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.engine('html', require('hogan-express'));
 
 // Other dependencies
 const arp = require('node-arp');
+const async = require('async');
 
 // Internal modules
 const strategy = require('./src/strategies/default');
@@ -53,9 +54,11 @@ app.post('/', (req, res) => {
 		},
 		// Get MAC address
 		(user, cb) => {
-			user.ip = req.ip;
+			user.ip = req.ip.replace(/^.*:/, '');
+			//user.ip
 
-			arp.getMAC(req.ip, (err, mac) => {
+			console.log("ARP for: " + user.ip);
+			arp.getMAC(user.ip, (err, mac) => {
 				if(err) {
 					console.error("There was an error while getting user's MAC address:\n" + err.stack);
 					cb(err);
@@ -68,7 +71,8 @@ app.post('/', (req, res) => {
 		},
 		// Add user to firewall rules
 		(user, cb) => {
-			firewall.authorize(mac, (err) => {
+			console.log("Authorizing MAC: " + user.mac);
+			firewall.authorize(user.mac, (err) => {
 				if(err) {
 					console.error("There was an error while adding user's MAC address to authorized addresses:\n" + err.stack);
 					cb(err);
@@ -78,32 +82,39 @@ app.post('/', (req, res) => {
 		}
 	], (err, data) => {
 		if(err) {
-			if(err instanceof Error)
-				res.json(err).sendStatus(500);
-			else {
+			if(err instanceof Error) {
 				res.json(err);
+				res.status(500);
+			} else {
 				switch(err.status) {
 					case 403:
 						return res.redirect('/?error=403');
 					default:
+						res.json(err);
 						res.status(500);
 						break;
 				}
 
 				res.end();
 			}
-		} else
-			res.redirect(req.body.redirect);
+		} else {
+			console.log("User authenticated: " + data.mac)
+
+			if(req.body.redirect)
+				res.redirect(req.body.redirect);
+			else
+				res.redirect("https://google.com");
+		}
 	});
 });
 
 app.listen(port, () => {
 	console.log('Setting firewall rules...');
-	//firewall.init((err) => {
-	//	if(err) {
-	//		console.error("Error initializing the firewall:\n" + err.stack);
-	//		process.exit(-1);
-	//	} else
+	firewall.init((err) => {
+		if(err) {
+			console.error("Error initializing the firewall:\n" + err.stack);
+			process.exit(-1);
+		} else
 			console.log(`Chillinode is now listening on port ${port}`);
-	//});
+	});
 });
--- src/strategies/default.js
+++ src/strategies/default.js
@@ -1,5 +1,6 @@
 "use strict"
 const passwd = require('passwd-linux');
+const async = require('async');
 
 module.exports = {
 	authenticate: (body, cb) => {
@@ -12,21 +13,26 @@ module.exports = {
 			// Check if user exists
 			(cb2) => {
 				passwd.checkUser(username, (err, res) => {
+					console.log("Res1: " + res);
+
 					if(err) {
 						console.error("There was an error while checking the existence of the user:\n" + err.stack);
 						cb2(err);
-					} else if(res === 'userExist')
-						cb(null, { username: username });
+					} else if(res == 'userExist')
+						cb2(null, { username: username });
 				});
 			},
 			// Check user password
 			(user, cb2) => {
 				passwd.checkPass(user.username, password, (err, res) => {
+					console.log("Res2: " + res);
+
 					if(err) {
 						console.error("There was an error while checking user's password:\n" + err.stack);
 						cb2(err);
-					} else
-						cb(null, { username: username, authenticated: (res === 'passwordCorrect') });
+					} else {
+						cb2(null, { username: username, authenticated: (res == 'passwordCorrect') });
+					}
 				});
 			}
 		], (err, user) => {
