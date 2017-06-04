bf106d42fd644a4824109937fdf01d30c3bb144b
Jakub Kerber <kerber.jg@gmail.com>
2017-06-05 01:05:39 +0200

    WORKS! FINALLY!

--- src/firewall.js
+++ src/firewall.js
@@ -13,7 +13,15 @@ function execCmd(str, cb) {
 		args: cmd.slice(1, cmd.length)
 	};
 
+	console.log(cmd);
 	const cp = child_process.spawn(cmd.exec, cmd.args);
+
+	cp.stderr.on('data', (data) => {
+		console.error(data.toString());
+	})
+	cp.stdout.on('data', (data) => {
+		console.log(data.toString());
+	})
     cp.on('close', (code) => {
         if(code !== 0)
             cb( new Error(`Process returned exit code: ${code}`) );
@@ -43,28 +51,39 @@ module.exports = {
 		async.series([
 			// Flushes the chains
 			(cb2) => {
-				execCmd("iptables -F", cb2);
+				let cmd = [
+					"iptables -F",
+					"iptables -X",
+					"iptables -t nat -F",
+					"iptables -t nat -X",
+					"iptables -t mangle -F",
+					"iptables -t mangle -X",
+					"iptables -t filter -F",
+					"iptables -t filter -X"
+				];
+
+				async.eachSeries(cmd, (cmd, cb3) => execCmd(cmd, cb3), cb2);
 			},
 			// Sets the rules
 			(cb2) => {
 				let cmd = [];
-				cmd.concat([
+				cmd = cmd.concat([
 					// Mangling rules (for MAC filtering)
 					"iptables -t mangle -N wlan0_Trusted",
 					"iptables -t mangle -N wlan0_Outgoing",
 					"iptables -t mangle -N wlan0_Incoming",
-					`iptables -t mangle -I PREROUTING 1 -i ${config.global.lanIf} -j wlan0_Outgoing`,
-					`iptables -t mangle -I PREROUTING 1 -i ${config.global.lanIf} -j wlan0_Trusted`,
-					`iptables -t mangle -I POSTROUTING 1 -o ${config.global.lanIf} -j wlan0_Incoming`,
+					`iptables -t mangle -I PREROUTING 1 -i ${global.config.lanIf} -j wlan0_Outgoing`,
+					`iptables -t mangle -I PREROUTING 1 -i ${global.config.lanIf} -j wlan0_Trusted`,
+					`iptables -t mangle -I POSTROUTING 1 -o ${global.config.lanIf} -j wlan0_Incoming`,
 					// Set NAT rules
 					"iptables -t nat -N wlan0_Outgoing",
 					"iptables -t nat -N wlan0_Router",
 					"iptables -t nat -N wlan0_Internet",
 					"iptables -t nat -N wlan0_UnknownDevices",
 					"iptables -t nat -N wlan0_WhitelistServers",
-					`iptables -t nat -A PREROUTING -i ${config.global.lanIf} -j wlan0_Outgoing`,
+					`iptables -t nat -A PREROUTING -i ${global.config.lanIf} -j wlan0_Outgoing`,
 					// Accepts connections to the router
-					`iptables -t nat -A wlan0_Outgoing -d ${routerIp} ACCEPT`,
+					`iptables -t nat -A wlan0_Outgoing -d ${routerIp} -j ACCEPT`,
 					// Accepts outgoing connections from authorized MACs
 					"iptables -t nat -A wlan0_Outgoing -j wlan0_Internet",
 					"iptables -t nat -A wlan0_Internet -m mark --mark 0x2 -j ACCEPT",
@@ -81,7 +100,7 @@ module.exports = {
 					"iptables -t filter -N wlan0_KnownDevices",
 					"iptables -t filter -N wlan0_UnknownDevices",
 					// Outgoing rules for forwarded packets
-					`iptables -t filter -I FORWARD -i ${config.global.lanIf} -j wlan0_Internet`,
+					`iptables -t filter -I FORWARD -i ${global.config.lanIf} -j wlan0_Internet`,
 					// Drop invalid packets
 					"iptables -t filter -A wlan0_Internet -m state --state INVALID -j DROP"
 					// Adapts segment size to avoid connection problems with big packets
@@ -95,7 +114,7 @@ module.exports = {
 					cmd.push(`iptables -t filter -A wlan0_WhitelistServers -d ${addr} -j ACCEPT`);
 				});
 
-				cmd.concat([
+				cmd = cmd.concat([
 					// Accept connections from authorized devices
 					"iptables -t filter -A wlan0_Internet -m mark --mark 0x2 -j wlan0_KnownDevices",
 					"iptables -t filter -A wlan0_KnownDevices -d 0.0.0.0/0 -j ACCEPT",
@@ -108,11 +127,13 @@ module.exports = {
 					"iptables -t filter -A wlan0_UnknownDevices -d 0.0.0.0/0 -p tcp --dport 67 -j ACCEPT",
 					// Reject everything else
 					"iptables -t filter -A wlan0_UnknownDevices -j REJECT --reject-with icmp-port-unreachable",
-					"iptables -t nat -A POSTROUTING -o eth0.2 -j MASQUERADE"
+					`iptables -t nat -A POSTROUTING -o ${global.config.wanIf} -j MASQUERADE`
 				]);
 
+				console.log(cmd);
+
 				// Applies the firewall settings
-				async.eachSeries(cmd, (cmd, cb3) => execCmd(cmd, cb3), c2);
+				async.eachSeries(cmd, (cmd, cb3) => execCmd(cmd, cb3), cb2);
 			}
 		], (err) => {
 			if(err)
@@ -131,7 +152,7 @@ module.exports = {
 
 	// Authorizes a device to access the network
 	authorize: (mac, cb) => {
-		execCmd(`iptables -t mangle -A wlan0_Outgoing -m mac --mac-source "${mac}" -j MARK --set-mark 2`, cb);
+		execCmd(`iptables -t mangle -A wlan0_Outgoing -m mac --mac-source ${mac} -j MARK --set-mark 2`, cb);
 	},
 
 	// Flushes the list of authenticated MACs
